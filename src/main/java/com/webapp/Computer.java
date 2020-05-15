287936bc4a00156b1517614a40d511369f92d759
LordCommander21 <gogadzedaniel21@gmail.com>
2020-05-15 17:34:33 +0400

    annotationsUpdated

--- src/main/java/com/webapp/Computer.java
+++ src/main/java/com/webapp/Computer.java
@@ -2,10 +2,18 @@ package com.webapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("singleton")
 public class Computer {
+    @Value("${computer.compId}")
+    private int id;
     private MusicPlayer musicPlayer;
     private MusicPlayer musicPlayerClassic;
     @Autowired
@@ -13,9 +21,31 @@ public class Computer {
         this.musicPlayer = musicPlayer;
         this.musicPlayerClassic = musicPlayerClassic;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     void compute(){
+        System.out.println("It's computer " + id);
         musicPlayer.playSong();
         System.out.println("");
         musicPlayerClassic.playSong();
+        System.out.println();
+    }
+
+    @PostConstruct
+    private void postConstr(){
+        System.out.println("Class constructed " + Computer.class.toString());
+    }
+
+    @Override
+    public String toString() {
+        return Computer.class.getName();
+    }
+
+    @PreDestroy
+    private void preDestr(){
+        System.out.println("Class destroyed" + Computer.class.toString());
     }
 }
--- src/main/java/com/webapp/Main.java
+++ src/main/java/com/webapp/Main.java
@@ -7,6 +7,9 @@ public class Main {
         ClassPathXmlApplicationContext context = new
                 ClassPathXmlApplicationContext("context.xml");
         Computer computer = context.getBean("computer",Computer.class);
+        Computer computer1 = context.getBean("computer",Computer.class);
+        computer1.setId(55);
+        computer1.compute();
         computer.compute();
         context.close();
     }
