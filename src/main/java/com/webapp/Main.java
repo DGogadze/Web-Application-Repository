94c06356b8412eddd9c638ad4daa68dc2d5eba4a
LordCommander21 <gogadzedaniel21@gmail.com>
2020-05-17 20:28:23 +0400

    upd

--- src/main/java/com/webapp/Main.java
+++ src/main/java/com/webapp/Main.java
@@ -1,11 +1,7 @@
 package com.webapp;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new
-                ClassPathXmlApplicationContext("context.xml");
-        context.close();
+
     }
 }
