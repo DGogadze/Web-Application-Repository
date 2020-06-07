103781de7628ec7a96923513c06f1bcec6e04121
LordCommander21 <gogadzedaniel21@gmail.com>
2020-06-07 16:56:55 +0400

    mvcApplication

---
+++ src/main/java/com/webapp/config/SpringConfig.java
@@ -0,0 +1,49 @@
+package com.webapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan("com.webapp")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+    private final ApplicationContext context;
+
+    @Autowired
+    SpringConfig(ApplicationContext applicationContext){
+        context=applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setApplicationContext(context);
+        springResourceTemplateResolver.setPrefix("/WEB-INF");
+        springResourceTemplateResolver.setSuffix(".html");
+        return springResourceTemplateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        springTemplateEngine.setEnableSpringELCompiler(true);
+        return springTemplateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry){
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(thymeleafViewResolver);
+    }
+}
