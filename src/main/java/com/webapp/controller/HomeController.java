36e2e1b380b33402536b4586638a5ef8e368cacd
LordCommander21 <gogadzedaniel21@gmail.com>
2020-06-08 18:17:33 +0400

    MVCControllers

--- src/main/java/com/webapp/controller/HomeController.java
+++ src/main/java/com/webapp/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("")
+    public String indexPage(){
+        return "index";
+    }
+
     @GetMapping("/home")
     public String homePage() {
         return "home/homePage";
