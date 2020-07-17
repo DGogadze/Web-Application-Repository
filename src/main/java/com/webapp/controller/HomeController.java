94b1c55981ee95ea06d63ee9a08931d5ee9b7a4a
LordCommander21 <gogadzedaniel21@gmail.com>
2020-07-17 14:16:48 +0400

    MVCParams

--- src/main/java/com/webapp/controller/HomeController.java
+++ src/main/java/com/webapp/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,7 +27,38 @@ public class HomeController {
 
     @GetMapping("/sayHello")
     public String sayHelloPage(@RequestParam(value = "name",required = false) String name,
-                               @RequestParam(value = "surname",required = false) String surname){
+                               @RequestParam(value = "surname",required = false) String surname,
+                               Model model){
+        model.addAttribute("message","Hello, " + name + " " + surname);
         return "home/sayHelloPage";
+    }
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam("x") int x,
+                                 @RequestParam("y") int y,
+                                 @RequestParam("action") String action,
+                                 Model model){
+
+        double result = 0;
+        String resultString = "Cannot divide to 0";
+        switch (action) {
+            case "multiply":
+                result=x*y;
+                break;
+            case "divide":
+                try {
+                    result=x/(double)y;
+                } catch (ArithmeticException ignored){
+
+                }
+                break;
+            case "subtraction":
+                    result = x-y;
+                    break;
+            case "addiction":
+                result=x+y;
+                break;
+        }
+        model.addAttribute("result",result);
+        return "home/calculatorPage";
     }
 }
