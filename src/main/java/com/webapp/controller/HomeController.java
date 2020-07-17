66295b9cc50ae04bf7f62820c737dd330a4e6020
LordCommander21 <gogadzedaniel21@gmail.com>
2020-07-17 13:26:32 +0400

    requstParams

--- src/main/java/com/webapp/controller/HomeController.java
+++ src/main/java/com/webapp/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -19,5 +22,11 @@ public class HomeController {
     @GetMapping("/exit")
     public String exitPage(){
         return "home/exitPage";
+    }
+
+    @GetMapping("/sayHello")
+    public String sayHelloPage(@RequestParam(value = "name",required = false) String name,
+                               @RequestParam(value = "surname",required = false) String surname){
+        return "home/sayHelloPage";
     }
 }
